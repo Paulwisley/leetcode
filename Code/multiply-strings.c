ffafde18474bd582815652b34d45041a044aee97
Paulwisley <1134556973@qq.com>
2018-10-17 00:28:47 +0800

    Undo

---
+++ Code/multiply-strings.c
@@ -0,0 +1,11 @@
+/*
+	@43 Medium
+	Multiply strings
+	Given two non-negative integers num1 and num2 represented as strings, 
+	return the product of num1 and num2, also represented as a string.
+	author：paul
+	date：
+*/
+string multiply(string num1, string num2) {
+
+}
