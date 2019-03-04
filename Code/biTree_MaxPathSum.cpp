616ba26ea057ab8c171fd8211e72c1ce09d88f0a
Paulwisley <1134556973@qq.com>
2019-03-04 23:18:53 +0800

    Hard 124

--- Code/biTree_MaxPathSum.cpp
+++ Code/biTree_MaxPathSum.cpp
@@ -10,23 +10,22 @@ struct TreeNode {
     TreeNode(int x) : val(x),left(NULL), right(NULL){}
 };
 
+int MAX = INT_MIN;
+
 int maxPathSum(TreeNode *root){
-    vector<int> nodes;
-    if(!root) return -1;
-    //traverse the bi tree, save the each node's val to vector
-    traverseTree(root, nodes);
-    for()
+    if(root == NULL)
+        return 0;
+    helper(root);
+    return MAX;
 }
 
-void traverseTree(TreeNode *root, vector<int> &nodes){
-    //default the tree is not empty
-    queue<TreeNode *>que;
-    que.push(root);
-    while (!que.empty()) {
-        TreeNode *tmp = que.front();
-        root.push_back(tmp->val);
-        que.pop();
-        if(tmp->left) que.push(tmp->left);
-        if(tmp->right) que.push(tmp->right);
+int helper(TreeNode *root){
+    if(!root){
+       return 0;
     }
+    int lnode = max(0, helper(root->left));
+    int rnode = max(0, helper(root->right));
+    MAX = max(root->val + lnode + rnode, MAX);
+    return max(0,max(lnode, rnode) + root->val);
 }
+
